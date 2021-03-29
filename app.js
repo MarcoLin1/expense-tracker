37e5b6cf8f245b7cb285ae40d9732ae79d56e230
Marco Lin <pohanlin0605@gmail.com>
2021-03-29 23:13:14 +0800

    feat: add handlbars

--- app.js
+++ app.js
@@ -15,13 +15,12 @@ db.once('open', () => {
   console.log('mongodb connected')
 })
 
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.render('index')
 })
 
 app.listen(port, () => {
