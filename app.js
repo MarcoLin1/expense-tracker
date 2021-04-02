6dde91af960d16190bf867cfc85f2ca29d3f8472
Marco Lin <pohanlin0605@gmail.com>
2021-04-03 00:54:06 +0800

    modify ui

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const Record = require('./models/record')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
@@ -11,8 +10,13 @@ require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
 app.use(bodyParser.urlencoded({ extends: true }))
+
 app.use(methodOverride('_method'))
+
 app.use(routes)
 
 app.listen(port, () => {
